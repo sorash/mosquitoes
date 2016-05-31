c65824e2eb87d4bf1e150739645a65614a0ae674
unknown <sashrafi3@SYT2107-W816.SeneDS.senecacollege.ca>
2016-05-31 18:23:11 -0400

    added renderer and changed file paths

--- src/main/java/com/sorash/mosquitoes/main/CommonProxy.java
+++ src/main/java/com/sorash/mosquitoes/main/CommonProxy.java
@@ -1,15 +1,25 @@
 package com.sorash.mosquitoes.main;
 
+import com.sorash.mosquitoes.entity.EntityMosquito;
+import com.sorash.mosquitoes.entity.ModelMosquito;
+import com.sorash.mosquitoes.entity.RenderMosquito;
+
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 public class CommonProxy 
 {
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
+    	// create the mosquito entity
+    	EntityRegistry.registerModEntity(EntityMosquito.class, "mosquito", 0, Mosquitoes.instance, 32, 32, false);
+    	
     }
     
     @EventHandler
@@ -20,5 +30,6 @@ public class CommonProxy
     @EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
+    	RenderingRegistry.registerEntityRenderingHandler(EntityMosquito.class, new RenderMosquito(Minecraft.getMinecraft().getRenderManager()));
     }
 }
--- src/main/java/com/sorash/mosquitoes/main/Resources.java
+++ src/main/java/com/sorash/mosquitoes/main/Resources.java
@@ -8,6 +8,6 @@ public class Resources
     public static final String VERSION = "1.0";
     
     // proxies
-    public static final String CLIENTPROXY = "com.sorash.mosquitoes.ClientProxy";
-    public static final String SERVERPROXY = "com.sorash.mosquitoes.ServerProxy";
+    public static final String CLIENTPROXY = "com.sorash.mosquitoes.main.ClientProxy";
+    public static final String SERVERPROXY = "com.sorash.mosquitoes.main.ServerProxy";
 }
