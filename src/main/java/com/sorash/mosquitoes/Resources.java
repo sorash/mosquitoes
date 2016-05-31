83b073766cef43fc3bbe23ba8ba87c64b38311a8
unknown <sashrafi3@SYT2107-W816.SeneDS.senecacollege.ca>
2016-05-31 16:18:34 -0400

    mod is born

---
+++ src/main/java/com/sorash/mosquitoes/Resources.java
@@ -0,0 +1,13 @@
+package com.sorash.mosquitoes;
+
+public class Resources 
+{
+	// mod info
+	public static final String MODID = "mosquitoes";
+	public static final String NAME = "Mosquitoes Mod";
+    public static final String VERSION = "1.0";
+    
+    // proxies
+    public static final String CLIENTPROXY = "com.sorash.mosquitoes.ClientProxy";
+    public static final String SERVERPROXY = "com.sorash.mosquitoes.ServerProxy";
+}
