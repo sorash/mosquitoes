ee09b8f65197b161e861b846920ce08f87ff8dbe
sorash <so.as67@yahoo.com>
2016-06-03 23:32:12 -0400

    added entity attributes

--- src/main/java/com/sorash/mosquitoes/entity/EntityMosquito.java
+++ src/main/java/com/sorash/mosquitoes/entity/EntityMosquito.java
@@ -1,6 +1,18 @@
 package com.sorash.mosquitoes.entity;
 
+import com.sorash.mosquitoes.main.Resources;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.IEntityLivingData;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.DamageSource;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 
 public class EntityMosquito extends EntityCreature 
@@ -10,4 +22,82 @@ public class EntityMosquito extends EntityCreature
 		super(worldIn);
 		this.setSize(0.15F, 0.15F);
 	}
+	
+    /**
+     * Returns the volume for the sounds this mob makes.
+     */
+    protected float getSoundVolume()
+    {
+        return 0.1F;
+    }
+    
+    /**
+     * Returns the sound this mob makes while it's alive.
+     */
+    protected String getLivingSound()
+    {
+        return Resources.MODID + ":sounds/mosquito.buzz";
+    }
+    
+    /**
+     * Returns true if this entity should push and be pushed by other entities when colliding.
+     */
+    public boolean canBePushed()
+    {
+        return false;
+    }
+    protected void collideWithEntity(Entity entity)
+    {
+    }
+
+    protected void collideWithNearbyEntities()
+    {
+    }
+    
+    /**
+     * returns if this entity triggers Block.onEntityWalking on the blocks they walk on. used for spiders and wolves to
+     * prevent them from trampling crops
+     */
+    protected boolean canTriggerWalking()
+    {
+        return false;
+    }
+    
+    public void fall(float distance, float damageMultiplier)
+    {
+    }
+    
+    protected void updateFallState(double y, boolean onGroundIn, Block blockIn, BlockPos pos)
+    {
+    }
+
+    /**
+     * Return whether this entity should NOT trigger a pressure plate or a tripwire.
+     */
+    public boolean doesEntityNotTriggerPressurePlate()
+    {
+        return true;
+    }
+    
+    /**
+     * Called when the entity is attacked.
+     */
+    public boolean attackEntityFrom(DamageSource source, float amount)
+    {
+    	// guarantee a one hit kill
+        return super.attackEntityFrom(source, 10F);
+    }
+    
+    public float getEyeHeight()
+    {
+        return this.height / 2.0F;
+    }
+    
+    /**
+     * Will return how many at most can spawn in a chunk at once.
+     */
+    public int getMaxSpawnedInChunk()
+    {
+        return 20;
+    }
 }
