2572742b862158dd2c3e42096deb861e4984f785
sorash <so.as67@yahoo.com>
2016-06-03 23:34:16 -0400

    added temporary spawns

--- src/main/java/com/sorash/mosquitoes/main/CommonProxy.java
+++ src/main/java/com/sorash/mosquitoes/main/CommonProxy.java
@@ -1,10 +1,11 @@
 package com.sorash.mosquitoes.main;
 
 import com.sorash.mosquitoes.entity.EntityMosquito;
-import com.sorash.mosquitoes.entity.ModelMosquito;
 import com.sorash.mosquitoes.entity.RenderMosquito;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -19,7 +20,21 @@ public class CommonProxy
     {
     	// create the mosquito entity
     	EntityRegistry.registerModEntity(EntityMosquito.class, "mosquito", 0, Mosquitoes.instance, 32, 32, false);
-    	
+
+    	// add spawn
+        // 3 numbers are as follows:
+    	// probability, min spawn count, max spawn count
+    	EntityRegistry.addSpawn(EntityMosquito.class, 300, 10, 20, EnumCreatureType.MONSTER, BiomeGenBase.plains);
+    	EntityRegistry.addSpawn(EntityMosquito.class, 300, 10, 20, EnumCreatureType.MONSTER, BiomeGenBase.beach);
+    	EntityRegistry.addSpawn(EntityMosquito.class, 300, 10, 20, EnumCreatureType.MONSTER, BiomeGenBase.deepOcean);
+    	EntityRegistry.addSpawn(EntityMosquito.class, 300, 10, 20, EnumCreatureType.MONSTER, BiomeGenBase.desert);
+    	EntityRegistry.addSpawn(EntityMosquito.class, 300, 10, 20, EnumCreatureType.MONSTER, BiomeGenBase.forest);
+    	EntityRegistry.addSpawn(EntityMosquito.class, 300, 10, 20, EnumCreatureType.MONSTER, BiomeGenBase.jungle);
+    	EntityRegistry.addSpawn(EntityMosquito.class, 300, 10, 20, EnumCreatureType.MONSTER, BiomeGenBase.swampland);
+    	EntityRegistry.addSpawn(EntityMosquito.class, 300, 10, 20, EnumCreatureType.MONSTER, BiomeGenBase.river);
+    	EntityRegistry.addSpawn(EntityMosquito.class, 300, 10, 20, EnumCreatureType.MONSTER, BiomeGenBase.roofedForest);
+    	EntityRegistry.addSpawn(EntityMosquito.class, 300, 10, 20, EnumCreatureType.MONSTER, BiomeGenBase.ocean);
+    	EntityRegistry.addSpawn(EntityMosquito.class, 300, 10, 20, EnumCreatureType.MONSTER, BiomeGenBase.jungleEdge);
     }
     
     @EventHandler
